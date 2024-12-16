a700e497127985df721676c6b2bc0e9d347f82c3
Nidhi Kulkarni <nidhivk09@gmail.com>
2024-12-16 10:14:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,11 @@ st.subheader("Upload an image of waste to classify it into one of 12 categories.
    
 
     # Upload and classify images
-uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-        st.write("Classifying...")
+#uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
+#if uploaded_file is not None:
+        #image = Image.open(uploaded_file)
+        #st.image(image, caption="Uploaded Image", use_column_width=True)
+        #st.write("Classifying...")
 
         
 
